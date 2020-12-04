5dcace133fd1b9b932d7d1530dd7f4166c345277
Yunsup Sim <pedogunu@gmail.com>
2020-12-04 16:38:07 +0900

    feat:Change package setting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
 module.exports = {
   poweredByHeader: false,
   trailingSlash: false,
-  env: {
-    API_PATH: process.env.API_PATH,
-  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(ts|tsx)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 };
