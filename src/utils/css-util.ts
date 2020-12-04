0617c1b90d8bf461d6d900d65efb0bd3366695d4
Yunsup Sim <pedogunu@gmail.com>
2020-12-04 16:39:59 +0900

    feat: Add css util and svg type

---
+++ src/utils/css-util.ts
@@ -0,0 +1,18 @@
+// mobile media query utils
+export const mobileQuery = '@media only screen and (max-width: 576px)';
+
+// font-family utils
+export const defaultFontFamily = '"NotoSansCJKKR", -apple-system, -moz-fixed, sans-serif';
+
+// font-color utils
+export const yellowColor = '#FFAB2E';
+
+export const mainColor = '#238BFE';
+
+export const mainFontColor = '#222222';
+
+export const greyFontColor = '#999999';
+
+export const greyBackgroundColor = '#EEEEEE';
+
+export const whiteColor = '#FFFFFF';
